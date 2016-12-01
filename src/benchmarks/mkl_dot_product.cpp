53215d73dd511a6433f21ee298d573bdab8b6e1a
Patricia Suriana <psuriana@google.com>
2016-12-01 09:59:14 -0800

    Add MKL dot product benchmark

--- src/benchmarks/mkl_dot_product.cpp
+++ src/benchmarks/mkl_dot_product.cpp
@@ -6,13 +6,16 @@
 #include <stdlib.h>
 #include <math.h>
 
-#include <cblas.h>
+#include <mkl.h>
+#include <mkl_cblas.h>
+#include <mkl_blas.h>
+#include <mkl_lapack.h>
+#include <mkl_lapacke.h>
 
 #include "benchmark.h"
 
-//g++ --std=c++11 -o dot_product dot_product.cpp -I/usr/local/opt/openblas/include -L/usr/local/opt/openblas/lib -lopenblas -lpthread
-
-//g++ --std=c++11 -o dot_product dot_product.cpp -I/usr/local/google/home/psuriana/OpenBLAS/installed/include -L/usr/local/google/home/psuriana/OpenBLAS/installed/lib -lopenblas -lpthread -fopenmp
+//g++ --std=c++11 -o mkl_dot_product mkl_dot_product.cpp -I/opt/intel/mkl/include -L/opt/intel/mkl/lib/intel64 -lmkl_intel_lp64 -lmkl_intel_thread -lmkl_core -L/opt/intel/lib/intel64_lin -liomp5 -lpthread -ldl -lm
+//LD_LIBRARY_PATH=/opt/intel/mkl/lib/intel64:/opt/intel/lib/intel64_lin:$LD_LIBRARY_PATH OMP_NUM_THREADS=8 numactl --cpunodebind=1 ./mkl_dot_product
 
 // dot product sdot
 #define N1 4
@@ -42,16 +45,11 @@ int main() {
 
     std::cout << "Done initializing\n";
 
-    openblas_set_num_threads(8);
-    std::cout << "Number of threads: " << openblas_get_num_threads() << "\n";
-    std::cout << "Number of processors: " << openblas_get_num_procs() << "\n";
-    std::cout << "Get parallel type: " << openblas_get_parallel() << "\n";
-
     std::cout << "Start benchmarking...\n";
     double t = benchmark(trials, iterations, [&]() {
         cblas_sdot(size, &(vec_A[0]), 1, &(vec_B[0]), 1);
     });
 
     float gbits = 32 * size * (2.0 / 1e9); // bits per seconds
-    printf("Dot-product cblas: %fms, %f Gbps\n", t * 1e3, (gbits / t));
+    printf("Dot-product mkl: %fms, %f Gbps\n", t * 1e3, (gbits / t));
 }
